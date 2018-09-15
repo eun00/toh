8bf0e9f7de9808eaeda8ec8b150d162a1dc7c05a
111 <quscogus@naver.com>
2018-09-15 14:59:52 +0900

    바인딩

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroseComponent } from './herose/herose.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HeroseComponent } from './herose/herose.component';
     HeroseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/herose/herose.component.ts
+++ src/app/herose/herose.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Hero} from '../hero';
+
 @Component({
   selector: 'app-herose',
   templateUrl: './herose.component.html',
   styleUrls: ['./herose.component.css']
 })
 export class HeroseComponent implements OnInit {
+hero:Hero = {
+  id:1,
+  name:"Windstorm"
+}
+  isSpecial = true;
 
-  constructor() { }
+  onSave(e:any){
+    console.log(e);
+    this.isSpecial = !this.isSpecial;
+  }
+
+  constructor() {
+    //window['hero']  = this.hero;
+  }
 
   ngOnInit() {
   }
